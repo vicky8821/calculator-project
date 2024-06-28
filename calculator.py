280bc4d3342a2db5f0e61641e1551a3e2bd0d2ca
vicky8821 <vicky_sharma8821@yahoo.com>
2024-06-28 16:53:11 -0400

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,13 @@
+# calculator.py
+class Calculator:
+    def add(self, a, b):
+        pass
+
+    def subtract(self, a, b):
+        pass
+
+    def multiply(self, a, b):
+        pass
+
+    def divide(self, a, b):
+        pass
